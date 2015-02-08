228c63244b71eaa3040dd26174f3568be8900530
divp <divp@localhost>
2015-02-08 11:11:45 +0200

    Sum function is correct for now

--- karatsuba.c
+++ karatsuba.c
@@ -6,7 +6,6 @@
 #define INT2CHAR(i)		((i) + 0x30)
 #define MAX_DIM			50
 
-
 typedef struct
 {
 	uint8_t dim;
@@ -18,9 +17,10 @@ void vl_init(char *str_number, verylong_t *number)
 	size_t len = strlen(str_number);
 	int8_t *ptr = number->numb;
 	number->dim = len;
+	memset(ptr, 0, MAX_DIM);
 	while(len > 0)
 	{
-		*ptr++ = str_number[len-1];
+		*ptr++ = CHAR2INT(str_number[len-1]);
 		--len;
 	}
 }
@@ -30,7 +30,7 @@ void vl_print(verylong_t *number)
 	uint8_t i;
 	for(i = number->dim; i > 0; i--)
 	{
-		putchar(number->numb[i-1]);
+		putchar(INT2CHAR(number->numb[i-1]));
 	}
 	putchar('\n');
 }
@@ -39,16 +39,18 @@ void vl_sum(verylong_t *op1, verylong_t *op2, verylong_t *result)
 {
 	uint8_t i = 0;
 	uint8_t sum_len = 0;
-	uint8_t res_len = 0;
 	uint8_t carry = 0;
 	
-	sum_len = op2->dim;
-	res_len = op1->dim;
 	if(op1->dim > op2->dim)
 	{
 		sum_len = op1->dim;
-		res_len = op2->dim;
 	}
+	else
+	{
+		sum_len = op2->dim;
+	}
+	
+	//printf("sum_len=%i\n", sum_len);
 
 	for(i = 0; i < sum_len; i++)
 	{
@@ -56,6 +58,7 @@ void vl_sum(verylong_t *op1, verylong_t *op2, verylong_t *result)
 		carry = 0;
 		if(result->numb[i] > 9)
 		{
+			//printf("%i\n", result->numb[i]);
 			carry = 1;
 			result->numb[i] -= 10;
 		}
@@ -63,21 +66,28 @@ void vl_sum(verylong_t *op1, verylong_t *op2, verylong_t *result)
 
 	if(carry != 0)
 	{
-		result->numb[i+1] += carry;
-		if(res_len < i+1)
+		//printf("carry after cycle %i\n", carry);
+		//printf("i=%i\n", i);
+		//printf("result->numb[i]=%i\n", result->numb[i]);
+		result->numb[i] += carry;
+		//printf("result->numb[i]=%i\n", result->numb[i]);
+		if(sum_len < i+1)
 		{
-			res_len += 1;
+			sum_len += 1;
 		}
 	}
 
-	result->dim = res_len;
+	result->dim = sum_len;
+	
+	//printf("result->dim=%i\n", result->dim);
 }
 
 int main(void)
 {
 	verylong_t num1, num2, result;
-	vl_init("1", &num1);
-	vl_init("3", &num2);
+	vl_init("123456789923456789", &num1);
+	vl_init("923456789678324098", &num2);
+	vl_init("0", &result);
 	vl_print(&num1);
 	vl_print(&num2);
 	vl_sum(&num1, &num2, &result);
