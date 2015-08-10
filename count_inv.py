09a9c83a7d76ee89626f7af98fb28ebbc84d39c3
divp <divp@localhost>
2015-08-10 20:52:46 +0300

    Exercise 2 done.

--- count_inv.py
+++ count_inv.py
@@ -1,6 +1,22 @@
 import sys
 
-assert len(sys.argv) == 2
+assert len(sys.argv) == 4
+
+def make_array(arr1, arr2):
+    res = [0] * len(arr1)
+    for i in range(len(arr1)):
+        position = arr1[i] - 1
+        value = arr2[i]
+        res[position] = value
+    return res
+
+def count_inversions(a):
+    res = 0
+    for i in range(len(a)):
+        for j in range(i+1, len(a)):
+            if a[i] > a[j]:
+                res += 1
+    return res
 
 with open(sys.argv[1], 'r') as file:
     ratings = []
@@ -10,8 +26,17 @@ with open(sys.argv[1], 'r') as file:
         ratings = line.split(' ')
         ratings = [int(x) for x in ratings]
         table.append(ratings[1:])
+    
+user1 = int(sys.argv[2]) - 1
+user2 = int(sys.argv[3]) - 1
 
-for i in range(10):
-    print(table[i])
+a = table[user1]
+b = table[user2]
+print(a)
+print(b)
+c = make_array(a, b)
+print(c)
+d = count_inversions(c)
+print(d)
 
     
