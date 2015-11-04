ccdcf34d70e1a431de9bf9296da33ca5b63308f8
divp <divp@localhost>
2015-11-04 21:14:05 +0200

    Exercise 5 done (previous commit with ex.5 was ex.4 actually).

--- heaps.c
+++ heaps.c
@@ -11,7 +11,7 @@ typedef struct
     int size;
 }Heap_t;
 
-void print_heap(Heap_t *heap, char *end);
+void print_heap(Heap_t *heap, char *end, int numb);
 void min_heapify(Heap_t *heap, int x);
 void max_heapify(Heap_t *heap, int x);
 
@@ -139,14 +139,92 @@ void min_heap_insert(Heap_t *heap, int key)
     min_heap_increase_key(heap, heap->size-1, key);
 }
 
-void print_heap(Heap_t *heap, char *end)
+void print_heap(Heap_t *heap, char *end, int numb)
 {
     int i;
-    for(i = 0; i < heap->size; i++)
+    if(numb <= 0)
+    {
+        numb = heap->size;
+    }
+    for(i = 0; i < numb; i++)
     {
         printf("%i ", heap->data[i]);
     }
     printf("%s", end);
+}
+
+void init_find_median(int max_size, Heap_t *heap_low, Heap_t *heap_high)
+{
+    int heap_size = max_size/2 + 2;
+    int *heap_low_mem = malloc(heap_size*sizeof(int));
+    int *heap_high_mem = malloc(heap_size*sizeof(int));
+    heap_low->data = heap_low_mem;
+    heap_high->data = heap_high_mem;
+    heap_low->size = 0;
+    heap_high->size = 0;
+}
+
+void del_find_median(Heap_t *heap_low, Heap_t *heap_high)
+{
+    free(heap_low->data);
+    free(heap_high->data);
+}
+
+int find_median(int new, Heap_t *heap_low, Heap_t *heap_high, int *m1, int *m2)
+{
+    int temp = 0;
+    int ret = 0;
+
+    if((heap_low->size > 0) && (new < heap_low->data[0]))
+    {
+        max_heap_insert(heap_low, new);
+    }
+    else if((heap_high->size > 0) && (new > heap_low->data[0]))
+    {
+        min_heap_insert(heap_high, new);
+    }
+    else
+    {
+        max_heap_insert(heap_low, new);
+    }
+
+    if(heap_low->size - heap_high->size > 1)
+    {
+        temp = heap_extract_max(heap_low);
+        min_heap_insert(heap_high, temp);
+    }
+    else if(heap_high->size - heap_low->size > 1)
+    {
+        temp = heap_extract_min(heap_high);
+        max_heap_insert(heap_low, temp);
+    }
+
+    if(heap_high->size == heap_low->size)
+    {
+        ret = 1;
+        if(heap_high->data[0] > heap_low->data[0])
+        {
+            *m2 = heap_high->data[0];
+            *m1 = heap_low->data[0];
+        }
+        else
+        {
+            *m1 = heap_high->data[0];
+            *m2 = heap_low->data[0];
+        }
+    }
+    else if(heap_high->size > heap_low->size)
+    {
+        ret = 0;
+        *m1 = heap_high->data[0];
+    }
+    else
+    {    
+        ret = 0; 
+        *m1 = heap_low->data[0];
+    }
+
+    return ret;
 }
 
 int main(int argc, const char *argv[])
@@ -160,11 +238,11 @@ int main(int argc, const char *argv[])
         my_heap.size = 10;
         my_heap.data = arr;
 
-        print_heap(&my_heap, "\n");
+        print_heap(&my_heap, "\n", 0);
         build_min_heap(&my_heap);
-        print_heap(&my_heap, "\n");
+        print_heap(&my_heap, "\n", 0);
         min_heap_insert(&my_heap, 1);
-        print_heap(&my_heap, "\n");
+        print_heap(&my_heap, "\n", 0);
     }
     else if(argc == 2)
     {
@@ -175,6 +253,7 @@ int main(int argc, const char *argv[])
         int new_numb = 0;
         int i = 0;
         int *arr;
+        int target = 2014;
 
         fp = fopen(argv[1], "r");
         fscanf(fp, "%i", &arr_len);
@@ -185,12 +264,45 @@ int main(int argc, const char *argv[])
             arr[i++] = new_numb;
         }
         
-        printf("%i\n", arr_len);
+        //printf("%i\n", arr_len);
+        //for(i = 0; i < arr_len; i++)
+        //{
+        //    printf("%i ", arr[i]);
+        //}
+        //printf("\n");
+
+        Heap_t heap_low, heap_high;
+        int m1, m2, ret;
+        init_find_median(arr_len, &heap_low, &heap_high);
         for(i = 0; i < arr_len; i++)
         {
-            printf("%i ", arr[i]);
+            ret = find_median(arr[i], &heap_low, &heap_high, &m1, &m2);
+            if(ret == 0)
+            {
+                if(i == target)
+                {
+                    printf("%i\n", m1);
+                    printf("Heap low: ");
+                    print_heap(&heap_low, "\n", 5);
+                    printf("Heap high: ");
+                    print_heap(&heap_high, "\n\n", 5);
+                }
+            }
+            else
+            {
+                if(i == target)
+                {
+                    printf("%i %i\n", m1, m2);
+                    printf("Heap low: ");
+                    print_heap(&heap_low, "\n", 5);
+                    printf("Heap high: ");
+                    print_heap(&heap_high, "\n\n", 5);
+                }
+            }
+            //print_heap(&heap_low, "\n", 0);
+            //print_heap(&heap_high, "\n\n", 0);
         }
-        printf("\n");
+        del_find_median(&heap_low, &heap_high);
         
         fclose(fp);
         free(arr);
